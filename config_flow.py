c02ae8e28b9774baf666798f522c88109af30971
nickpdawson <nd@nickdawson.net>
2023-08-03 17:00:23 -0600

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,32 +1,41 @@
-"""Config flow for Exist.io."""
-import logging
-from homeassistant import config_entries
-from homeassistant.helpers import config_entry_oauth2_flow
 import voluptuous as vol
 
-from .const import DOMAIN
+from homeassistant import config_entries
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from .const import DOMAIN, CONF_TOKEN
 
-
-class OAuth2FlowHandler(
-    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
-):
-    """Config flow to handle Exist.io OAuth2 authentication."""
-
-    DOMAIN = DOMAIN
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
-
-    @property
-    def logger(self) -> logging.Logger:
-        """Return logger."""
-        return logging.getLogger(__name__)
+class ExistIoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Exist.io."""
 
     async def async_step_user(self, user_input=None):
-        """Handle a flow start."""
+        """Handle the initial step."""
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            return self.async_create_entry(title="Exist.io", data=user_input)
 
-        fields = {
-            vol.Required("token"): str,
-        }
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_TOKEN): str}),
+            errors=errors,
+        )
 
-        return self.async_show_form(step_id="user", data_schema=vol.Schema(fields))
+    async def async_setup_entry(self, entry):
+        """Set up an Exist.io entry."""
+        coordinator = get_coordinator(self.hass, entry)
+        self.hass.data[DOMAIN] = coordinator
+        return await coordinator.async_refresh()
+
+def get_coordinator(hass, entry):
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="Exist.io sensor",
+        update_interval=timedelta(hours=1),
+    )
+    coordinator.update = lambda: update_data(hass, coordinator, entry)
+    return coordinator
+
+async def update_data(hass, coordinator, entry):
+    exist_io = ExistIO(hass, entry.data[CONF_TOKEN])
+    data = await exist_io.async_update()
+    return data
